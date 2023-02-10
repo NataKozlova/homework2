f05088920acd148a9dbcb5ca632448fcaf59321d
natakozlova <natabalabanova555@yandex.ru>
2023-02-10 13:04:47 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_000;
+        int  mileForRub= 50;
+        int miles = ticketPrice / mileForRub;
+
+
+        System.out.println("miles: " + miles);
+    }
+}
